fc2280daa499e59cede4c772dbd2f88c1c43c302
Steve Budarz <sbudarz@gmail.com>
2020-06-29 15:43:48 -0400

    modularized the auth flow

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from 'react';
 
-const Login = (props) => {
+const Login = () => {
   return (
     <div>
       <h1>Welcome to FreeTutorLink!</h1>
-      <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77iojwzvr5axo9&redirect_uri=http://localhost:3000/login/authCode&scope=r_liteprofile%20r_emailaddress">
-        Login with LinkedIn
-      </a>
+      <a href='/login'>Login with LinkedIn</a>
     </div>
   );
 };
