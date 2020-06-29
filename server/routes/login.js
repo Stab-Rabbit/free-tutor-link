59c7c6d650b325540bee9fe9382e802d299f8e61
Kevin <ruan8111@gmail.com>
2020-06-29 17:01:02 -0400

    middleware rewrite

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
 
-router.get('/authCode', loginController.getAccessToken);
+// router.get('/authCode', loginController.getAccessToken);
 router.get('/', loginController.sendToLinkedIn);
+router.get('/authCode', 
+  loginController.getAccessToken, 
+  loginController.requestAccessToken, 
+  loginController.setCookie, 
+  loginController.getNameAndPicture, 
+  loginController.getUserEmail, 
+  loginController.getAccessToken, 
+  (req, res) => {
+    return res.status(200).redirect('http://localhost:8080/home');
+})
 
 module.exports = router;
+
